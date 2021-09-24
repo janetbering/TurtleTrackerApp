a873ddf451fc3b8a55c02440d99c66da536c5e51
Janet Bering <jb321@duke.edu>
2021-09-24 14:07:19 -0400

    add new script to repo

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 14:02:14 2021
+
+@author: jb321
+"""
+
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Janet Bering (jb321@duke.edu)
+# Date:   Fall 2021
+#--------------------------------------------------------------
